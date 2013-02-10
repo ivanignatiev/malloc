a2fd30ee1966e8f8f4123385277d8a21ebe90bdd
Oleg Kuznietsov <oleg.kuznetsov@epitech.eu>
2013-02-10 17:25:35 +0000

    spaces -> tabulations (where needed)
    git-svn-id: svn+ssh://koala-scm.epitech.net/malloc-2016-ignati_i@4 36cb5d8d-1174-49d2-8aaf-2f7ad866023b

--- malloc.h
+++ malloc.h
@@ -5,22 +5,22 @@
 ** Login   <ignati_i@epitech.net>
 ** 
 ** Started on  Thu Jan 31 17:13:28 2013 Ivan Ignatiev
-** Last update Sun Feb 10 16:35:05 2013 ivan ignatiev
+** Last update Sun Feb 10 18:23:37 2013 Kuznietsov Oleg
 */
 
 #ifndef MALLOC_H_
 # define MALLOC_H_
 
-# include    <unistd.h>
+# include <unistd.h>
 
 void	*malloc(size_t size);
-void    *calloc(size_t num, size_t size);
-void    *realloc(void *ptr, size_t size);
-void    free(void *ptr);
-void    show_alloc_mem(void);
+void	*calloc(size_t num, size_t size);
+void	*realloc(void *ptr, size_t size);
+void	free(void *ptr);
+void	show_alloc_mem(void);
 void	show_alloc_mem_detail(void);
-void    *valloc(size_t size);
-void    *memalign(size_t boundary, size_t size);
-int     posix_memalign(void **memptr, size_t aligment, size_t size);
+void	*valloc(size_t size);
+void	*memalign(size_t boundary, size_t size);
+int	posix_memalign(void **memptr, size_t aligment, size_t size);
 
 #endif /* !MALLOC_H_ */
